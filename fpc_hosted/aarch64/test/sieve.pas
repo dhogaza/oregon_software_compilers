3f4365e795c9d6a292fea277f2b3f2602b106896
Don Baccus <dhogaza@gmail.com>
2023-06-19 10:24:43 -0700

    added quicksort and sieve examples

---
+++ fpc_hosted/aarch64/test/sieve.pas
@@ -0,0 +1,34 @@
+{$noindexcheck,norangecheck,nopointercheck,nostackcheck}
+{$nowalkback}
+ 
+%include 'testlib';
+
+{       Sieve of Eratosthenes benchmark      }
+ 
+program sieve;
+const
+  SIZE = 8190;
+  ITERMAX = 10;
+
+var
+  flags : array [0..SIZE] of boolean;
+  i, prime, k, count, iter : integer;
+begin
+  for iter := 1 to ITERMAX do begin
+    count := 0;
+    for i := 0 to SIZE do 
+       flags[i] := true;
+    for i := 0 to SIZE do
+      if flags[i] then begin
+        prime := i + i + 3;
+        k := i + prime;
+        while k <= SIZE do begin
+          flags[k] := false;
+          k := k + prime;
+        end; { while }
+        count := count + 1;
+      end; { if } 
+  end; { for }
+{  writeln(count,' iterations.');}
+end. { sieve }
+
