7739d28581684b5ba39cd713d1f2edd3cd6ca18b
Don Baccus <dhogaza@gmail.com>
2023-07-05 19:17:59 -0700

    Added C versions of sieve to demonstrate both gcc and clang kicking my butt.

---
+++ fpc_hosted/aarch64/test/sieve.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define  SIZE 1000000
+#define  ITERMAX 1000
+
+int flags[SIZE + 1];
+
+void p() {
+  int i, prime, k, count, iter;
+
+  for (iter = 0; iter < ITERMAX; iter++) {
+    count = 0;
+    for (i = 0; i <= SIZE; i++) { 
+       flags[i] = 1;
+    }
+    for (i = 0;  i <= SIZE; i++) {
+      if (flags[i]) {
+        prime = i + i + 3;
+        k = i + prime;
+        while (k <= SIZE) {
+          flags[k] = 0;
+          k = k + prime;
+        }
+        count = count + 1;
+      }
+    }
+  }
+  printf("count: %d\n", count);
+}
+
+int main() {
+  p();
+  return 0;
+}
+
