c448bdda3bf4623dd6520958aaaabe6c396bbc6e
Don Baccus <dhogaza@gmail.com>
2026-05-29 15:21:00 -0700

    Changed benchmark script to use -Mdelphi with the fpc compile since Free Pascal STILL defaults to 16 bit integers!

--- regression/aarch64/benchmarkone.sh
+++ regression/aarch64/benchmarkone.sh
@@ -9,7 +9,7 @@ $pasdir/pas2arm64 $src/$1 --include=$lib --noch --mac=$1
 gcc $HOME/oregon_software_compilers/lib/darwin/paslib.o $1.s
 echo "timing pascal2 version..."
 time ./a.out
-fpc $src/$1 -FU./ -o./a.out -O3
+fpc $src/$1 -FU./ -o./a.out -O3 -Mdelphi
 echo "timing free pascal version ..."
 time ./a.out
 rm -f a.out *.tmp
