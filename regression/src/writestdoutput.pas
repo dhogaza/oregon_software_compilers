c8e5d3a46ef46e4ff862af97712f2ab3c3a38c98
Don Baccus <dhogaza@gmail.com>
2026-06-18 10:45:52 -0700

    Decided I should have a simple test of the writing of extended strings in these tests.

--- regression/src/writestdoutput.pas
+++ regression/src/writestdoutput.pas
@@ -3,14 +3,16 @@
 var i:integer;
   ch: char;
   a: packed array [1..3] of char;
+  s: string[255];
 
 begin
   a := 'xyz';
   write('y', 'z');
   i := 3;
+  s := a;
   write('a':i);
   write(100:4);
   writeln('!');
-  writeln('abc':1, a:4);
+  writeln('abc':1, a:4, s:4);
   writeln(false, true, false:1, true:1, false: 10, true: 10);
 end.
