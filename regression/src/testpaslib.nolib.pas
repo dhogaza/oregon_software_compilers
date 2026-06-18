a01a943c9ec4b14e6812182ee4d689955299b9dd
Don Baccus <dhogaza@gmail.com>
2026-06-18 10:39:34 -0700

    Added writestdoutput test to this test and verified output is identical.  Needs more simple tests.

--- regression/src/testpaslib.nolib.pas
+++ regression/src/testpaslib.nolib.pas
@@ -1,4 +1,22 @@
 %include 'paslib'
 {$main}
+
+{ regression testing script will flag any differendes in assembly
+  output, which may or may not represent bugs in the generated code.
+  So we will do some simple testing of the library here, was well.
+}
+
+var i:integer;
+  ch: char;
+  a: packed array [1..3] of char;
+
 begin
+  a := 'xyz';
+  write('y', 'z');
+  i := 3;
+  write('a':i);
+  write(100:4);
+  writeln('!');
+  writeln('abc':1, a:4);
+  writeln(false, true, false:1, true:1, false: 10, true: 10);
 end.
