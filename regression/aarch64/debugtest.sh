a599c0360238da5d464ffd725a0e931d95536a7a
Don Baccus <dhogaza@gmail.com>
2026-06-30 02:34:21 -0700

    Added new script to simply compile with --mac --noch --test to check for things like registers that have a usage count that is nonzero at the end of a procedure.

---
+++ regression/aarch64/debugtest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "testing --mac --noch --test compile only"
+. ./env.sh
+for f in $src/*.pas; do
+  echo "--- testing $(basename $f) ---"
+  . ./fileattrs.sh $f
+  pushd $os >/dev/null
+  if [[ "$type" == "pas" || "$type" == "nolib" ]]; then
+    $pasdir/pas2arm64 $f --include=$lib --noch --mac=$base --test
+  fi
+  popd >/dev/null
+done
