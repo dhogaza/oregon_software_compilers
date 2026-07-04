bd9e3a921eb97aa228e4a77e91602856d6741991
Don Baccus <dhogaza@gmail.com>
2026-07-04 02:21:09 -0700

    Added more file stuff to files.pas, library changed so nolib assembly output changed, sets of greater than 8 but no more than 32 elements assigned to a halfword or word rather than quad.

--- regression/src/files.pas
+++ regression/src/files.pas
@@ -3,6 +3,8 @@ var f: file of char;
 
 begin
 
+  writeln('write, read, and print foo.txt'); writeln;
+
   rewrite(f, 'foo.txt', ':w+');
 
   for ch := 'a' to 'z' do
@@ -18,6 +20,8 @@ begin
     end;
   writeln;
 
+  writeln('rewrite, read, and print foo.txt'); writeln;
+
   rewrite(f);
   for ch := 'z' downto 'a' do
     begin
@@ -32,6 +36,20 @@ begin
     end;
   writeln;
 
+  writeln('delete foo.txt'); writeln;
+
   delete(f);
+
+  writeln; writeln('read and print ../env.h'); writeln;
+
+  reset(f, '../env.sh');
+  while not eof(f) do
+    begin
+    write(f^);
+    get(f);
+    end;
+
+  writeln; writeln('try to do a put on the read-only file ../env.sh'); writeln;
+  put(f);
 
 end.
