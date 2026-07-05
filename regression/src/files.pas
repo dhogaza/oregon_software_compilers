9b6402bfa859f7d5b0f9afd3d2e1c0ba0b15929a
Don Baccus <dhogaza@gmail.com>
2026-07-05 04:21:30 -0700

    Added seek to library, added seek test to files.pas.

--- regression/src/files.pas
+++ regression/src/files.pas
@@ -36,6 +36,26 @@ begin
     end;
   writeln;
 
+  seek(f, 2);
+  f^ := '!';
+  put(f);
+  reset(f);
+  while not eof(f) do
+    begin
+    write(f^);
+    get(f);
+    end;
+  writeln;
+
+  seek(f,4);
+  while not eof(f) do
+    begin
+    write(f^);
+    get(f);
+    end;
+  writeln;
+
+
   writeln('delete foo.txt'); writeln;
 
   delete(f);
