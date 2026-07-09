7fa5d52f2f75b39526ba55260ba4153b68cccd50
Don Baccus <dhogaza@gmail.com>
2026-07-09 08:25:11 -0700

    Added integer write/read from file to the filestext test and since the library chaged, the nolib assemgly output did as well.

--- regression/src/filestext.pas
+++ regression/src/filestext.pas
@@ -1,8 +1,11 @@
 var f:text;
   ch: char;
+  i,j: integer;
   s: string[255];
 
 begin
+
+  writeln('rewrite foo.txt, write text, reset, read/print char buffer'); writeln;
   rewrite(f, 'foo.txt',':w+');
   writeln(f, 'abc','a':3, 42);
   writeln(f, 'how about that?');
@@ -20,14 +23,46 @@ begin
     writeln('"');
     end;
 
-  writeln(f, 'append to the end');
+  writeln; writeln('append, reset, read and print using string buffer'); writeln;
 
+  writeln(f, 'append to the end');
   reset(f);
   while not eof(f) do
     begin
     write('"');
     readln(f, s);
     writeln(s,'"');
+    end;
+
+  writeln; writeln('rewrite, write integers, read/print'); writeln;
+
+  rewrite(f);
+  writeln(f, 123, 456);
+  writeln(f);
+  writeln(f, 654, 321);
+
+  reset(f);
+  while not eof(f) do
+    begin
+    readln(f, i, j);
+    writeln(i,j);
+    end;
+
+  writeln; writeln('rewrite, write csv integers, read/print'); writeln;
+
+  rewrite(f);
+  writeln(f, 123:1, ',', 456:1);
+  writeln(f, 654:1, ',', 321:1);
+
+  reset(f);
+  while not eof(f) do
+    begin
+    read(f, i);
+    write(i:1);
+    read(f, ch);
+    write(ch);
+    readln(f, i);
+    writeln(i:1);
     end;
 
 end.
