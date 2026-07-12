bb1eb604ca24711df5dc8bc55a42517539aac473
Don Baccus <dhogaza@gmail.com>
2026-07-12 10:36:01 -0700

    Initialize filevar for standard files.

--- lib/paslib.pas
+++ lib/paslib.pas
@@ -738,6 +738,7 @@ begin
   filep^.filename := '(standard input)';
   filep^.streamp := _p_inputstream;
   filep^.bufferp := ref(filep^.ch);
+  filep^.filevar := loophole(_p_addressptr, ref(input));
   filep^.status := [_p_perm, _p_read, _p_text];
 
   { define standard output file }
@@ -745,6 +746,7 @@ begin
   filep^.filename := '(standard output)';
   filep^.streamp := _p_outputstream;
   filep^.bufferp := ref(filep^.ch);
+  filep^.filevar := loophole(_p_addressptr, ref(output));
   filep^.status := [_p_perm, _p_write, _p_def, _p_text];
 
   { define standard error file }
@@ -752,6 +754,7 @@ begin
   filep^.filename := '(standard error)';
   filep^.streamp := _p_errorstream;
   filep^.bufferp := ref(filep^.ch);
+  filep^.filevar := loophole(_p_addressptr, ref(stderror));
   filep^.status := [_p_perm, _p_write, _p_def, _p_text];
 end;
 
