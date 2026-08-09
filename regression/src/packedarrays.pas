1c100a2360250fb7a1343e46cf70cd826345b2ef
Don Baccus <dhogaza@gmail.com>
2026-08-09 14:33:05 -0700

    Now that packed arrays work somewhat, updated test.

--- regression/src/packedarrays.pas
+++ regression/src/packedarrays.pas
@@ -1,3 +1,5 @@
+procedure bar;
+
 var
   r: packed record
        k: 0..7;
@@ -8,19 +10,21 @@ var
   a: packed array [0..9] of boolean;
   a1: packed array [0..9] of 0..15;
 
-procedure foo; external;
 procedure foo;
 begin
+  r.k := 3;
   r.b[i].a := false;
   r.b[i].b := true;
   r.b[i].c := true;
   r.b[i].d := false;
   a[i] := true; a[i + 1] := false;
-  a1[i] := 0; a1[i + 1] := 7; a1[i + 2] := 15;
+  a1[i] := 0; a1[i + 1] := 5; a1[i + 2] := 15;
 end;
+
 begin
   i := 1;
   foo;
+  writeln('r.k expect 3 ', r.k);
   writeln('r.b[i].a expect false ', r.b[i].a);
   writeln('r.b[i].b expect true ', r.b[i].b);
   writeln('r.b[i].c expect true ', r.b[i].c);
@@ -28,6 +32,49 @@ begin
   writeln('a[i] expect true ', a[i]);
   writeln('a[i + 1] expect false ', a[i + 1]);
   writeln('a1[i] expect 0' , a1[i]);
-  writeln('a1[i + 1] expect 7', a1[i + 1]);
+  writeln('a1[i + 1] expect 5', a1[i + 1]);
   writeln('a1[i + 2] expect 15', a1[i + 2]);
+  for i := 0 to 9 do a[i] := odd(i);
+  for i := 0 to 9 do
+    writeln('a[', i:1, '] expect ', odd(i), a[i]:7);
+end;
+
+var
+  r: packed record
+       k: 0..7;
+       b: packed array[0..9] of
+            packed record a,b,c,d: boolean end;
+     end;
+  i: integer;
+  a: packed array [0..9] of boolean;
+  a1: packed array [0..9] of 0..15;
+
+procedure foo;
+begin
+  r.k := 3;
+  r.b[i].a := false;
+  r.b[i].b := true;
+  r.b[i].c := true;
+  r.b[i].d := false;
+  a[i] := true; a[i + 1] := false;
+  a1[i] := 0; a1[i + 1] := 5; a1[i + 2] := 15;
+end;
+
+begin
+  bar;
+  i := 1;
+  foo;
+  writeln('r.k expect 3 ', r.k);
+  writeln('r.b[i].a expect false ', r.b[i].a);
+  writeln('r.b[i].b expect true ', r.b[i].b);
+  writeln('r.b[i].c expect true ', r.b[i].c);
+  writeln('r.b[i].d expect false ', r.b[i].d);
+  writeln('a[i] expect true ', a[i]);
+  writeln('a[i + 1] expect false ', a[i + 1]);
+  writeln('a1[i] expect 0' , a1[i]);
+  writeln('a1[i + 1] expect 5', a1[i + 1]);
+  writeln('a1[i + 2] expect 15', a1[i + 2]);
+  for i := 0 to 9 do a[i] := odd(i);
+  for i := 0 to 9 do
+    writeln('a[', i:1, '] expect ', odd(i), a[i]:7);
 end.
